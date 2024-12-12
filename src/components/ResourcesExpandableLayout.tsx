c998baefc574644abbad3ca1c0329e4eb684446c
Miunn <remi@remicaulier.fr>
2024-12-12 17:41:40 +0100

    Scrolling content

--- src/components/ResourcesExpandableLayout.tsx
+++ src/components/ResourcesExpandableLayout.tsx
@@ -3,6 +3,7 @@ import React, { ReactNode, useEffect, useId, useRef, useState } from "react";
 import { AnimatePresence, motion } from "framer-motion";
 import { useOutsideClick } from "@/hooks/use-outside-click";
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "./ui/card";
+import { ScrollArea } from "./ui/scroll-area";
 
 export type ResourceCardType = {
     title: string;
@@ -93,19 +94,21 @@ export default function ExpandableCardDemo() {
                                     <motion.p layoutId={`text-${active.text}-${id}`}>
                                         {active.text}
                                     </motion.p>
-                                    <div className="pt-4 relative">
-                                        <motion.div
-                                            layout
-                                            initial={{ opacity: 0 }}
-                                            animate={{ opacity: 1 }}
-                                            exit={{ opacity: 0 }}
-                                            className="text-neutral-600 text-xs md:text-sm lg:text-base h-40 md:h-fit pb-10 flex flex-col items-start gap-4 overflow-auto dark:text-neutral-400 [mask:linear-gradient(to_bottom,white,white,transparent)] [scrollbar-width:none] [-ms-overflow-style:none] [-webkit-overflow-scrolling:touch]"
-                                        >
-                                            {typeof active.content === "function"
-                                                ? active.content()
-                                                : active.content}
-                                        </motion.div>
-                                    </div>
+                                    <ScrollArea className="h-72 w-full">
+                                        <div className="pt-4 relative">
+                                            <motion.div
+                                                layout
+                                                initial={{ opacity: 0 }}
+                                                animate={{ opacity: 1 }}
+                                                exit={{ opacity: 0 }}
+                                                className="text-neutral-600 text-xs md:text-sm lg:text-base h-40 md:h-fit pb-10 flex flex-col items-start gap-4 overflow-auto dark:text-neutral-400 [scrollbar-width:none] [-ms-overflow-style:none] [-webkit-overflow-scrolling:touch]"
+                                            >
+                                                {typeof active.content === "function"
+                                                    ? active.content()
+                                                    : active.content}
+                                            </motion.div>
+                                        </div>
+                                    </ScrollArea>
                                 </CardContent>
                                 <CardFooter>
                                     <motion.button
