7b979927550c6d974a8910acb2d636ca5196f93f
Miunn <remi@remicaulier.fr>
2024-12-14 22:55:04 +0100

    Number ticker resources counter

--- src/app/resources/page.tsx
+++ src/app/resources/page.tsx
@@ -2,6 +2,7 @@ import { getResources } from "@/actions/resources";
 import CreateResourceDialog from "@/components/resources/CreateResourceDialog";
 import ResourcesExpandableLayout, { ResourceCardType } from "@/components/ResourcesExpandableLayout";
 import GridPattern from "@/components/ui/grid-pattern";
+import NumberTicker from "@/components/ui/number-ticker";
 import { cn } from "@/lib/utils";
 import { Metadata } from "next";
 
@@ -17,7 +18,7 @@ export default async function Resources() {
         <div className="relative w-full max-w-6xl mx-auto overflow-x-hidden mb-52">
             <div className="mt-52 mb-32 flex justify-between items-start">
                 <div>
-                    <h1 className="tracking-wide text-2xl">Resources</h1>
+                    <h1 className="tracking-wide text-2xl"><NumberTicker value={resources.length} /> Resource{resources.length == 1 ? "" : "s"}</h1>
                     <p>Latest news about cybersecurity</p>
                 </div>
                 <CreateResourceDialog />
