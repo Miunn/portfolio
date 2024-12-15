b7fb5d02d52ed955d6c3a30d35e4660a76437da2
Miunn <remi@remicaulier.fr>
2024-12-15 11:48:39 +0100

    Fix overflow issue

--- src/app/resources/page.tsx
+++ src/app/resources/page.tsx
@@ -15,7 +15,7 @@ export default async function Resources() {
     const resources = await getResources();
 
     return (
-        <div className="relative w-full max-w-6xl mx-auto overflow-x-hidden mb-52">
+        <div className="relative w-full max-w-6xl mx-auto overflow-hidden mb-52">
             <div className="mt-52 mb-32 flex justify-between items-start">
                 <div>
                     <h1 className="tracking-wide text-2xl"><NumberTicker value={resources.length} /> Resource{resources.length == 1 ? "" : "s"}</h1>
