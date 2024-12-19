10eb2a3f99240b768f32b4a4c79efb8ec3f2edfb
Miunn <remi@remicaulier.fr>
2024-12-19 13:37:03 +0100

    Delete resource server action

--- src/actions/resources.ts
+++ src/actions/resources.ts
@@ -26,6 +26,23 @@ export async function createResource(title: string, description: string, text: s
     return { status: "ok" };
 }
 
+export async function deleteResource(id: number) {
+    
+    if (!(await isAuthenticated())) {
+        return { status: "Unauthorized", message: "You must be logged in to create a new resource" }
+    }
+
+    await prisma.resource.delete({
+        where: {
+            id: id
+        }
+    });
+
+    revalidatePath("/resources");
+
+    return { status: "ok" }
+}
+
 export async function getResources() {
 
     const resources = await prisma.resource.findMany({
