e7d3e65be727c3fcbfda2cdb20a1d85933ffb67d
Rémi Caulier <remi@remicaulier.fr>
2026-01-31 14:08:37 +0100

    feat: resume, internship

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,60 +6,73 @@ import SkillsList from "@/skills/SkillsList";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
-    title: 'Rémi Caulier',
-}
+	title: "Rémi Caulier",
+};
 
 export default function Home() {
-    return (
-        <>
-            <Particles
-                className="absolute inset-0"
-                quantity={500}
-                ease={80}
-                color={"#ffffff"}
-                refresh
-            />
-            <main className="max-w-6xl relative w-full overflow-hidden flex min-h-screen flex-col items-center mx-auto px-10">
-                <section className={"relative w-full max-w-6xl flex min-h-dvh flex-col justify-center"}>
-                    <h1 className={"tracking-wide text-2xl"}>Hi, I&apos;m Rémi.</h1>
-                    <p>
-                        I'm a fifth year cyber-security student from France.<br />
-                        I'm pursuing an engineering degree at INSA Hauts-de-France.<br />
-                        Passionate about IT and cybersecurity, I enjoy strengthening information system security<br />
-                        and developing projects with a security-by-design approach.
-                    </p>
-                </section>
+	return (
+		<>
+			<Particles
+				className="absolute inset-0"
+				quantity={500}
+				ease={80}
+				color={"#ffffff"}
+				refresh
+			/>
+			<main className="max-w-6xl relative w-full overflow-hidden flex min-h-screen flex-col items-center mx-auto px-10">
+				<section
+					className={
+						"relative w-full max-w-6xl flex min-h-dvh flex-col justify-center"
+					}
+				>
+					<h1 className={"tracking-wide text-2xl"}>
+						Hi, I&apos;m Rémi.
+					</h1>
+					<p className={"max-w-2xl"}>
+						I'm a computer science and cybersecurity graduate.
+						<br />
+						I pursued an engineering degree at INSA Hauts-de-France.
+						<br />
+						Passionate about IT and cybersecurity, I enjoy
+						strengthening information system security and developing
+						projects with a security-by-design approach.
+					</p>
+				</section>
 
-                <div className="relative w-full">
-                    <Particles
-                        className="absolute inset-0 h-full"
-                        quantity={150}
-                        ease={80}
-                        color={"#ffffff"}
-                        refresh
-                    />
-                    <section className={"max-w-6xl mx-auto mb-16"}>
-                        <h1 className={"tracking-wide text-2xl mb-8"}>Experiences</h1>
+				<div className="relative w-full">
+					<Particles
+						className="absolute inset-0 h-full"
+						quantity={150}
+						ease={80}
+						color={"#ffffff"}
+						refresh
+					/>
+					<section className={"max-w-6xl mx-auto mb-16"}>
+						<h1 className={"tracking-wide text-2xl mb-8"}>
+							Experiences
+						</h1>
 
-                        <ExperienceGrid />
-                    </section>
-                </div>
-                <section className={"w-full max-w-6xl mb-16"}>
-                    <h1 className={"tracking-wide text-2xl mb-8"}>Achievements and leisure activities</h1>
+						<ExperienceGrid />
+					</section>
+				</div>
+				<section className={"w-full max-w-6xl mb-16"}>
+					<h1 className={"tracking-wide text-2xl mb-8"}>
+						Achievements and leisure activities
+					</h1>
 
-                    <ProjectsGrid />
-                </section>
-                <section className={"w-full max-w-6xl mb-16"}>
-                    <h1 className={"tracking-wide text-2xl mb-8"}>Education</h1>
+					<ProjectsGrid />
+				</section>
+				<section className={"w-full max-w-6xl mb-16"}>
+					<h1 className={"tracking-wide text-2xl mb-8"}>Education</h1>
 
-                    <EducationTracing />
-                </section>
-                <section className={"w-full max-w-6xl mb-16"}>
-                    <h1 className={"tracking-wide text-2xl mb-8"}>Skills</h1>
+					<EducationTracing />
+				</section>
+				<section className={"w-full max-w-6xl mb-16"}>
+					<h1 className={"tracking-wide text-2xl mb-8"}>Skills</h1>
 
-                    <SkillsList />
-                </section>
-            </main>
-        </>
-    );
+					<SkillsList />
+				</section>
+			</main>
+		</>
+	);
 }
--- src/components/magicui/avatar-circles.tsx
+++ src/components/magicui/avatar-circles.tsx
@@ -6,39 +6,43 @@ import { cn } from "@/lib/utils";
 import Image from "next/image";
 
 interface AvatarCirclesProps {
-    className?: string;
-    numPeople?: number;
-    avatarUrls: string[];
+	className?: string;
+	numPeople?: number;
+	avatarUrls: string[];
 }
 
 const AvatarCircles = ({
-    numPeople,
-    className,
-    avatarUrls,
+	numPeople,
+	className,
+	avatarUrls,
 }: AvatarCirclesProps) => {
-    return (
-        <div className={cn("z-10 flex -space-x-4 rtl:space-x-reverse", className)}>
-            {avatarUrls.map((url, index) => (
-                <Image
-                    key={index}
-                    className="h-10 w-10 rounded-full border-2 border-white dark:border-gray-800 bg-background"
-                    src={url}
-                    width={40}
-                    height={40}
-                    alt={`Avatar ${index + 1}`}
-                />
-            ))}
-            {numPeople && numPeople > 0 && (
-
-                <a
-                    className="flex h-10 w-10 items-center justify-center rounded-full border-2 border-white bg-black text-center text-xs font-medium text-white hover:bg-gray-600 dark:border-gray-800 dark:bg-white dark:text-black"
-                    href=""
-                >
-                    +{numPeople}
-                </a>
-            )}
-        </div>
-    );
+	return (
+		<div
+			className={cn(
+				"z-10 flex -space-x-4 rtl:space-x-reverse",
+				className,
+			)}
+		>
+			{avatarUrls.map((url, index) => (
+				<Image
+					key={index}
+					className="h-10 w-10 rounded-full border-2 border-white dark:border-gray-800 bg-background object-contain"
+					src={url}
+					width={40}
+					height={40}
+					alt={`Avatar ${index + 1}`}
+				/>
+			))}
+			{numPeople && numPeople > 0 && (
+				<a
+					className="flex h-10 w-10 items-center justify-center rounded-full border-2 border-white bg-black text-center text-xs font-medium text-white hover:bg-gray-600 dark:border-gray-800 dark:bg-white dark:text-black"
+					href=""
+				>
+					+{numPeople}
+				</a>
+			)}
+		</div>
+	);
 };
 
 export default AvatarCircles;
