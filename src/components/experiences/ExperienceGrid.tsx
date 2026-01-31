98e878fe68ebb9caff54c85d05588eef79037aac
Rémi Caulier <remi@remicaulier.fr>
2026-01-31 14:11:21 +0100

    feat: resume, internship (#15)

--- src/components/experiences/ExperienceGrid.tsx
+++ src/components/experiences/ExperienceGrid.tsx
@@ -1,58 +1,127 @@
-import {Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle} from "@/components/ui/card";
+import {
+	Card,
+	CardContent,
+	CardDescription,
+	CardFooter,
+	CardHeader,
+	CardTitle,
+} from "@/components/ui/card";
 import AvatarCircles from "@/components/magicui/avatar-circles";
 
 export default function ExperienceGrid() {
-    return (
-        <div className={"grid grid-cols-1 lg:grid-cols-2 gap-10 z-10"}>
-            <Card className={"flex flex-col bg-background z-20"}>
-                <CardHeader>
-                    <CardTitle>
-                        Fixed term contract at <span className={"text-green-500"}>Elico - Regis Martelet</span>
-                    </CardTitle>
-                    <CardDescription>July - August 2024</CardDescription>
-                </CardHeader>
-                <CardContent className={"flex-grow flex"}>
-                    <p>
-                        Front-end and back-end rebuild of an existing inventory management web application.
-                    </p>
-                </CardContent>
-                <CardFooter>
-                    <AvatarCircles avatarUrls={["images/php.svg", "/images/symfony-light.png", "/images/twig.svg", "/images/react.svg"]} />
-                </CardFooter>
-            </Card>
-            <Card className={"flex flex-col bg-background z-20"}>
-                <CardHeader>
-                    <CardTitle>
-                        Internship at <span className={"text-green-500"}>Elico - Regis Martelet</span>
-                    </CardTitle>
-                    <CardDescription>September 2023 - January 2024</CardDescription>
-                </CardHeader>
-                <CardContent className={"flex-grow flex"}>
-                    <p>
-                        Development of CRM web application from scratch to meet company needs.
-                    </p>
-                </CardContent>
-                <CardFooter>
-                    <AvatarCircles avatarUrls={["images/php.svg", "/images/symfony-light.png", "/images/twig.svg"]} />
-                </CardFooter>
-            </Card>
-            <Card className={"flex flex-col bg-background z-20"}>
-                <CardHeader>
-                    <CardTitle>
-                        Internship at <span className={"text-green-500"}>Pandrol - BLONDEL Group</span>
-                    </CardTitle>
-                    <CardDescription>July - August 2021</CardDescription>
-                </CardHeader>
-                <CardContent className={"flex-grow flex"}>
-                    <p>
-                        Development of a native Android application involving Bluetooth communications with sensors to accurately measure rail alignment during automated process.<br/>
-                        The project was carried out for the R&D department.
-                    </p>
-                </CardContent>
-                <CardFooter>
-                    <AvatarCircles avatarUrls={["images/android-studio.svg", "/images/java.svg"]} />
-                </CardFooter>
-            </Card>
-        </div>
-    )
+	return (
+		<div className={"grid grid-cols-1 lg:grid-cols-2 gap-10 z-10"}>
+			<Card className={"flex flex-col bg-background z-20"}>
+				<CardHeader>
+					<CardTitle>
+						Software Engineering Internship at{" "}
+						<span className={"text-green-500"}>
+							Techguys - Montréal, QC, Canada
+						</span>
+					</CardTitle>
+					<CardDescription>March - September 2025</CardDescription>
+				</CardHeader>
+				<CardContent className={"flex-grow flex"}>
+					<p>
+						Software engineering internship focused on developing
+						web applications using NextJS, system design, database
+						structuring and cloud deployment.
+					</p>
+				</CardContent>
+				<CardFooter>
+					<AvatarCircles
+						avatarUrls={[
+							"/images/react.svg",
+							"/images/tailwind.png",
+							"/images/nextjs.svg",
+							"/images/google-cloud.png",
+						]}
+					/>
+				</CardFooter>
+			</Card>
+			<Card className={"flex flex-col bg-background z-20"}>
+				<CardHeader>
+					<CardTitle>
+						Fixed term contract at{" "}
+						<span className={"text-green-500"}>
+							Elico - Regis Martelet
+						</span>
+					</CardTitle>
+					<CardDescription>July - August 2024</CardDescription>
+				</CardHeader>
+				<CardContent className={"flex-grow flex"}>
+					<p>
+						Front-end and back-end rebuild of an existing inventory
+						management web application.
+					</p>
+				</CardContent>
+				<CardFooter>
+					<AvatarCircles
+						avatarUrls={[
+							"images/php.svg",
+							"/images/symfony-light.png",
+							"/images/twig.svg",
+							"/images/react.svg",
+						]}
+					/>
+				</CardFooter>
+			</Card>
+			<Card className={"flex flex-col bg-background z-20"}>
+				<CardHeader>
+					<CardTitle>
+						Internship at{" "}
+						<span className={"text-green-500"}>
+							Elico - Regis Martelet
+						</span>
+					</CardTitle>
+					<CardDescription>
+						September 2023 - January 2024
+					</CardDescription>
+				</CardHeader>
+				<CardContent className={"flex-grow flex"}>
+					<p>
+						Development of CRM web application from scratch to meet
+						company needs.
+					</p>
+				</CardContent>
+				<CardFooter>
+					<AvatarCircles
+						avatarUrls={[
+							"images/php.svg",
+							"/images/symfony-light.png",
+							"/images/twig.svg",
+						]}
+					/>
+				</CardFooter>
+			</Card>
+			<Card className={"flex flex-col bg-background z-20"}>
+				<CardHeader>
+					<CardTitle>
+						Internship at{" "}
+						<span className={"text-green-500"}>
+							Pandrol - BLONDEL Group
+						</span>
+					</CardTitle>
+					<CardDescription>July - August 2021</CardDescription>
+				</CardHeader>
+				<CardContent className={"flex-grow flex"}>
+					<p>
+						Development of a native Android application involving
+						Bluetooth communications with sensors to accurately
+						measure rail alignment during automated process.
+						<br />
+						The project was carried out for the R&D department.
+					</p>
+				</CardContent>
+				<CardFooter>
+					<AvatarCircles
+						avatarUrls={[
+							"images/android-studio.svg",
+							"/images/java.svg",
+						]}
+					/>
+				</CardFooter>
+			</Card>
+		</div>
+	);
 }
--- src/components/projects/ProjectsGrid.tsx
+++ src/components/projects/ProjectsGrid.tsx
@@ -1,15 +1,20 @@
 import ProjectCard from "@/components/projects/ProjectCard";
-import {Fragment} from "react";
+import { Fragment } from "react";
 
 export default function ProjectsGrid() {
-    return (
-        <div className={"w-full space-y-10"}>
-            {projects.map((project, index) => (
-                <Fragment key={project.title}>
-                    <ProjectCard title={project.title} description={project.description} icon={project.icon} period={project.period}/>
-                </Fragment>
-            ))}
-            {/*<div className={"relative min-h-96"}>
+	return (
+		<div className={"w-full space-y-10"}>
+			{projects.map((project, index) => (
+				<Fragment key={project.title}>
+					<ProjectCard
+						title={project.title}
+						description={project.description}
+						icon={project.icon}
+						period={project.period}
+					/>
+				</Fragment>
+			))}
+			{/*<div className={"relative min-h-96"}>
                 <Card className={"absolute top-0 left-0 z-10 w-2/3"}>
                     <CardHeader>
                         <CardTitle>HebergOS</CardTitle>
@@ -75,49 +80,54 @@ export default function ProjectsGrid() {
                     </div>
                 </div>
             </div>*/}
-        </div>
-    )
+		</div>
+	);
 }
 
 const projects = [
-    {
-        title: "./insash",
-        description: "Co-founder of the INSA Hauts-de-France computer science association. " +
-            "The association's goal is to offer somewhere to talk, work and share about IT. " +
-            "Together, we built several projects and particapated in different events.",
-        icon: "/images/insash.png",
-        period: "September 2022 - Present"
-    },
-    {
-        title: "Cross-INSA CTF",
-        description: "Participation in the Capture The Flag contest, a cyber-security competition between INSA schools. " +
-            "The goal is to solve challenges in different categories such as cryptography, web, reverse engineering, forensic and realistic. " +
-            "First 2 teams of each school are qualified for the final.<br/>" +
-            "Our team placed 3rd in qualifications and 6th in finals. ",
-        icon: "/images/cyber_insa.png",
-        period: "February 2024"
-    },
-    {
-        title: "Cod'INSA",
-        description: "Cod'INSA is an algorithmic contest between INSA schools. " +
-            "The goal during qualifications is to individually solve several algorithmic problems in a limited time and the top 5 qualifies for the final. " +
-            "During the final, in group of 5, the goal is to build a bot within 20 hours on a game specially designed for the final. Each bot of each school fights against each other to determine the winner. <br/>" +
-            "3 times finalist.",
-        icon: "/images/codinsa.png",
-        period: "April 2021, April 2022 and April 2023"
-    },
-    {
-        title: "HebergOS",
-        description: "Co-Developped with a friend, HebergOS is a software that enables automatic deployment of clustered projects using Docker and Symfony. " +
-            "Through the web interface, users can create, manage and deploy their projects in a few clicks. " +
-            "They have access to a dashboard to monitor their projects and resources. A terminal is also available to interact with the docker instance.",
-        icon: "/images/hebergos.png",
-        period: "Built from December 2023 to January 2024"
-    },
-    {
-        title: "Ensembll",
-        description: "A car parks management web application. For users to book their parking places and administrators to manage car parks, bookings, users and stats.",
-        icon: "/images/ensembll-cover.png",
-        period: "Built from November 2022 to August 2023"
-    }
-]
+	{
+		title: "./insash",
+		description:
+			"Co-founder of the INSA Hauts-de-France computer science association. " +
+			"The association's goal is to offer somewhere to talk, work and share about IT. " +
+			"Together, we built several projects and particapated in different events.",
+		icon: "/images/insash.png",
+		period: "September 2022 - March 2025",
+	},
+	{
+		title: "Cross-INSA CTF",
+		description:
+			"Participation in the Capture The Flag contest, a cyber-security competition between INSA schools. " +
+			"The goal is to solve challenges in different categories such as cryptography, web, reverse engineering, forensic and realistic. " +
+			"First 2 teams of each school are qualified for the final.<br/>" +
+			"Our team placed 3rd in qualifications and 6th in finals. ",
+		icon: "/images/cyber_insa.png",
+		period: "February 2024",
+	},
+	{
+		title: "Cod'INSA",
+		description:
+			"Cod'INSA is an algorithmic contest between INSA schools. " +
+			"The goal during qualifications is to individually solve several algorithmic problems in a limited time and the top 5 qualifies for the final. " +
+			"During the final, in group of 5, the goal is to build a bot within 20 hours on a game specially designed for the final. Each bot of each school fights against each other to determine the winner. <br/>" +
+			"3 times finalist.",
+		icon: "/images/codinsa.png",
+		period: "April 2021, April 2022 and April 2023",
+	},
+	{
+		title: "HebergOS",
+		description:
+			"Co-Developped with a friend, HebergOS is a software that enables automatic deployment of clustered projects using Docker and Symfony. " +
+			"Through the web interface, users can create, manage and deploy their projects in a few clicks. " +
+			"They have access to a dashboard to monitor their projects and resources. A terminal is also available to interact with the docker instance.",
+		icon: "/images/hebergos.png",
+		period: "Built from December 2023 to January 2024",
+	},
+	{
+		title: "Ensembll",
+		description:
+			"A car parks management web application. For users to book their parking places and administrators to manage car parks, bookings, users and stats.",
+		icon: "/images/ensembll-cover.png",
+		period: "Built from November 2022 to August 2023",
+	},
+];
